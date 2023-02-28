42a1255c66397a04b3fc88e43f2907cce761c5fa
Badreddine Hadjoudj <b.hadjoudj@live.fr>
2023-02-28 13:44:40 +0100

    dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,2 @@
-type User = {
-    username: string,
-    lastname: string
-    }
-    
-    const john: User = {
-    username: 'hadj mas3oud',
-    lastname: 'hadj'
-    }
-
-    console.log('abdlekader ya bou3lam')
-    console.log('zad lhal 3lia')
-    
-    console.log(john)
+console.log(process.env.NODE_ENV)
+console.log(process.env.NODE_ENV)
