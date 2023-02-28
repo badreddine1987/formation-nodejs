53dcf4de758ae1196d3c889b31af5f89215cb7de
Badreddine Hadjoudj <b.hadjoudj@live.fr>
2023-02-28 10:11:51 +0100

    currently

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 type User = {
-    username: string
-}
+    username: string,
+    lastname: string
+    }
+    
+    const john: User = {
+    username: 'hadj mas3oud',
+    lastname: 'hadj'
+    }
 
-
-const john: User = {
-    username: 'hadj mas3oud'
-}
-
-console.log(john)
+    console.log('abdlekader ya bou3lam')
+    console.log('zad lhal 3lia')
+    
+    console.log(john)
